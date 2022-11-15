be1907d7f996fea8f37cbac3267a63f31ae16282
Allen Xu <allxu@nvidia.com>
2022-11-15 18:22:21 +0800

    refine
    Signed-off-by: Allen Xu <allxu@nvidia.com>

--- k8s/read-s3-test.py
+++ k8s/read-s3-test.py
@@ -7,6 +7,6 @@ spark = SparkSession.builder.appName("s3-read-test").getOrCreate()
 data_path = "s3a://tpcds-share-emr/sf10-parquet/useDecimal=true,useDate=true,filterNull=false/call_center"
 column_name = "cc_call_center_id"
 spark.read.parquet(data_path).select(column_name).show()
-# sleep 100 seconds to allow user to check the Spark UI
+# sleep 300 seconds to allow user to check the Spark UI
 time.sleep(300)
 spark.stop()
